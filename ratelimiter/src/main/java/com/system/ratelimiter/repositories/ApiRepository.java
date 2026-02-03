51cdbeb7085939304ac1fcdc563a17d4bcb0a07b
SIDHARTH <codefather0003@gmail.com>
2026-02-03 09:08:18 +0530

    Test Commit

--- ratelimiter/src/main/java/com/system/ratelimiter/repositories/ApiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.system.ratelimiter.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.system.ratelimiter.models.ApiKey;
-
-public interface ApiRepository extends JpaRepository<ApiKey, Long> {
-    public ApiKey findByApiKey(String apiKey);
-}

0274030c4b5bc00880ff8933c40e966f28876bc0
SIDHARTH <codefather0003@gmail.com>
2026-02-24 14:25:00 +0530

    JUST FOR A FUN

--- distributed-rate-limiter-backend/src/main/java/com/system/ratelimiter/RatelimiterApplication.java
+++ distributed-rate-limiter-backend/src/main/java/com/system/ratelimiter/RatelimiterApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RatelimiterApplication {
+	private static final String DEFAULT_LOCAL_DB_URL =
+			"jdbc:mysql://localhost:3306/ratelimiter_db?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
 	public static void main(String[] args) {
 		normalizeCloudEnvironment();
@@ -27,18 +29,35 @@ public class RatelimiterApplication {
 		if (dbUrl != null) {
 			String normalized = normalizeDbUrl(dbUrl);
 			System.setProperty("DB_URL", normalized);
+			System.setProperty("spring.datasource.url", normalized);
+			System.setProperty("jakarta.persistence.jdbc.url", normalized);
 
 			if (isBlank(firstNonBlank("DB_DRIVER"))) {
 				if (normalized.startsWith("jdbc:postgresql:")) {
 					System.setProperty("DB_DRIVER", "org.postgresql.Driver");
+					System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
 				} else if (normalized.startsWith("jdbc:mysql:")) {
 					System.setProperty("DB_DRIVER", "com.mysql.cj.jdbc.Driver");
+					System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
 				}
 			}
+		} else if (isBlank(firstNonBlank("spring.datasource.url", "jakarta.persistence.jdbc.url"))) {
+			System.setProperty("spring.datasource.url", DEFAULT_LOCAL_DB_URL);
+			System.setProperty("jakarta.persistence.jdbc.url", DEFAULT_LOCAL_DB_URL);
 		}
 
-		setIfMissing("DB_USERNAME", firstNonBlank("DB_USERNAME", "PGUSER", "DATABASE_USER", "POSTGRES_USER"));
-		setIfMissing("DB_PASSWORD", firstNonBlank("DB_PASSWORD", "PGPASSWORD", "DATABASE_PASSWORD", "POSTGRES_PASSWORD"));
+		String user = firstNonBlank("DB_USERNAME", "PGUSER", "DATABASE_USER", "POSTGRES_USER");
+		String pass = firstNonBlank("DB_PASSWORD", "PGPASSWORD", "DATABASE_PASSWORD", "POSTGRES_PASSWORD");
+		setIfMissing("DB_USERNAME", user);
+		setIfMissing("DB_PASSWORD", pass);
+		if (!isBlank(user)) {
+			System.setProperty("spring.datasource.username", user);
+			System.setProperty("jakarta.persistence.jdbc.user", user);
+		}
+		if (!isBlank(pass)) {
+			System.setProperty("spring.datasource.password", pass);
+			System.setProperty("jakarta.persistence.jdbc.password", pass);
+		}
 	}
 
 	private static void normalizeRedisConfig() {
